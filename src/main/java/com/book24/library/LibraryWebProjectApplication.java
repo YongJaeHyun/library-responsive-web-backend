67aa0f8c50be121eabbfa271cbb1810acc26ed04
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-03-26 09:40:48 +0900

    메인 실행 함수

---
+++ src/main/java/com/book24/library/LibraryWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.book24.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebProjectApplication.class, args);
+	}
+
+}
